9926f18aa1d4ab11bae8e0e11d138b3302e746e1
arun <a.kothapalli@reply.com>
2020-12-04 21:38:02 +0000

    cart dropdown

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,7 +2,6 @@ import CollectionItem from '../collection-item/collection-item.component'
 import './collection-preview.styles.scss'
 
 const CollectionPreview = ({title, items}) => {
-    console.log({title, items})
   return (
     <div className="collection-preview">
       <h1 className="title" >{title.toUpperCase()}</h1>
